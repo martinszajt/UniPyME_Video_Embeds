79e2cf731ddeb9fdea354d303dfe32c0ec497e7f
Martin Szajt <58597855+martinszajt@users.noreply.github.com>
2023-05-10 11:18:30 -0300

    Update example.js

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 
    // Wistia Video
-  const wistiaVideoId = "fe8502f5qi";
+  const wistiaVideoId = "df2872v5dr";
   
   
   let wistiaEmbed = document.getElementById("wistia_embed");
@@ -25,11 +25,41 @@
       video.bind("timechange", function (t) {
         
           // PopUp Spects
-        let openSecond = 3;
-        let closeSecond = 6;
+        let openSecond = 20;
+        let closeSecond = 30;
+        let surveyURL = "https://es.surveymonkey.com/r/8JCJZD3";
+        let surveyHeight = '500px';
+        let surveyWidth = '600px';
+        
+        //Do-Not change
+        if (t > openSecond && t < closeSecond) {
+          if (!activeSurvey) {
+            iframe.style.height = surveyHeight;
+            iframe.style.width = surveyWidth;
+            activeSurvey = surveyURL;
+            iframe.src = surveyURL;
+            iframe_container.classList.add("visible");
+          }
+        }
+        if (t > closeSecond || t < openSecond) {
+          if (activeSurvey === surveyURL) {
+            activeSurvey = null;
+            iframe.src =
+              "https://unipymelatam.com/survey-callback";
+            iframe_container.classList.remove("visible");
+          }
+        }
+      });
+       
+       // Survey 2
+      video.bind("timechange", function (t) {
+        
+          // PopUp Spects
+        let openSecond = 35;
+        let closeSecond = 50;
         let surveyURL = "https://es.surveymonkey.com/r/ZZZ5GB9";
-        let surveyHeight = '300px';
-        let surveyWidth = '300px';
+        let surveyHeight = '500px';
+        let surveyWidth = '600px';
         
         //Do-Not change
         if (t > openSecond && t < closeSecond) {
